79d1a1ea6cc49739409ff288f532568239d4389a
kuwaqata <victor_rudakov@mail.ru>
2021-04-10 10:03:23 +0300

    Library Unit tests

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -8,7 +8,7 @@ class MyLogTest extends TestCase
 
     public function testWrite()
     {
-        $this->assertEquals('', MyLog::write());
+        $this->assertEquals('', MyLog::write("!@#$%"));
         $this->assertEquals('', MyLog::write(4586));
         $this->assertEquals('', MyLog::write("Rudakov"));
 
@@ -24,7 +24,6 @@ class MyLogTest extends TestCase
     public function testMyLogEx()
     {
         $this->expectException(TypeError::class);
-        $this->assertEquals('', MyLog::log());
         $this->assertEquals('', MyLog::log(null));
     }
 }
