29b9c85b847b20b82e371ec07c2bcce3f4d9268d
kuwaqata <victor_rudakov@mail.ru>
2021-04-11 19:41:17 +0300

    Library Unit tests

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -5,17 +5,16 @@ use PHPUnit\Framework\TestCase;
 
 class MyLogTest extends TestCase
 {
+	public function testLog() 
+	{
+        $this->expectOutputString("Rudakov231");
+        MyLog::log("Rudakov231");
+        MyLog::write();
+    }
 	
 	public function testInst() 
 	{
         $this->assertInstanceOf(MyLog::class, MyLog::Instance());
     }
 
-    public function testWriteLog() 
-	{
-        $this->expectOutputString("Rudakov231");
-        MyLog::log("Rudakov231");
-        MyLog::write();
-    }
-    
 }
